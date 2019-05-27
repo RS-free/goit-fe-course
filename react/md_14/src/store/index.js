c055a9336b90d4b6efc3e6f7f23a33cec720cff6
AleksandraHrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2019-05-27 12:17:59 +0300

    in progress

--- react/md_14/src/store/index.js
+++ react/md_14/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducers from './rootReducers';
 
-const Store = createStore(rootReducers);
+const Store = createStore(rootReducers, devToolsEnhancer());
 
 export default Store;
